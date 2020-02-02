8b107b778ae96cf98e759068258232d933902a61
Rahul <rahul.rahulmishra11@gmail.com>
2020-02-02 20:34:44 +0530

    create seeder class is CreateUserSeeder

---
+++ database/seeds/CreateUsersSeeder.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Seeder;
+use App\User;
+
+class CreateUsersSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $user =[
+              [
+              	'name' => 'Admin',
+              	'email'=>'rahul.rahulmishra11@gmail.com',
+              	'is_admin' =>'1',
+              	'password' =>bcrypt('12345678'),
+              ],
+               [
+              	'name' => 'user',
+              	'email'=>'rahul.rahulmishra94@gmail.com',
+              	'is_admin' =>'0',
+              	'password' =>bcrypt('12345678'),
+              ],
+             
+        ];
+
+        foreach ($user as $key => $value){
+        	User::create($value);
+        }
+    }
+}
